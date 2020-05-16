973284635e0df62e92127de7114bc454e4dad070
yuelchen <ychen.workspace@gmail.com>
2020-05-15 22:05:03 -0400

    Updated AWS Lambda function for copying and uploading S3 object

--- s3-object-to-s3-lambda/src/main/java/com/aws/handler/S3ClientHandler.java
+++ s3-object-to-s3-lambda/src/main/java/com/aws/handler/S3ClientHandler.java
@@ -1,11 +1,14 @@
 package com.aws.handler;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.ylc.log.AppLog;
@@ -41,5 +44,49 @@ public class S3ClientHandler {
 		return false;
 	}
 	
+	public static boolean uploadObject(String location, String bucketName, String objectPrefix) {
+		try {
+			File file = new File(location);
+			
+			PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectPrefix, file);
+			client.putObject(putObjectRequest);
+			
+			file.delete();
+			return true;
+			
+		} catch(AmazonS3Exception e) {
+			AppLog.log.error("Unable to upload file from '" + location + "' to 's3://" + bucketName + "/" + objectPrefix + "' due to AmazonS3Exception: " + e.getMessage() + "; caused by " + e.getCause());
+		} catch(SecurityException e) {
+			AppLog.log.error("Unable to upload file from '" + location + "' to 's3://" + bucketName + "/" + objectPrefix + "' due to SecurityException: " + e.getMessage() + "; caused by " + e.getCause());
+		}
+		
+		return false;
+    }
 	
+	public static boolean uploadObjectAES256(String location, String bucketName, String objectPrefix) throws AmazonS3Exception, IOException {
+		try {
+			FileInputStream fileInputStream = new FileInputStream(location);
+
+	        ObjectMetadata objectMetadata = new ObjectMetadata();
+	        objectMetadata.setSSEAlgorithm(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
+
+	        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectPrefix, fileInputStream, objectMetadata);
+	        client.putObject(putObjectRequest);
+	        
+	        fileInputStream.close();
+	        
+	        File file = new File(location);
+	        file.delete();
+	        return true;
+	        
+		} catch(AmazonS3Exception e) {
+			AppLog.log.error("Unable to upload file from '" + location + "' to 's3://" + bucketName + "/" + objectPrefix + "' with AES256 due to AmazonS3Exception: " + e.getMessage() + "; caused by " + e.getCause());
+		} catch(SecurityException e) {
+			AppLog.log.error("Unable to upload file from '" + location + "' to 's3://" + bucketName + "/" + objectPrefix + "' with AES256 due to SecurityException: " + e.getMessage() + "; caused by " + e.getCause());
+		} catch(IOException e) {
+			AppLog.log.error("Unable to upload file from '" + location + "' to 's3://" + bucketName + "/" + objectPrefix + "' with AES256 due to IOException: " + e.getMessage() + "; caused by " + e.getCause());
+		}
+
+		return false;
+    }
 }
--- s3-object-to-s3-lambda/src/main/java/com/ylc/main/S3ToS3Lambda.java
+++ s3-object-to-s3-lambda/src/main/java/com/ylc/main/S3ToS3Lambda.java
@@ -16,7 +16,7 @@ public class S3ToS3Lambda {
 		String eventType = S3EventHandler.getEventType(event);
 		String sourceBucket = S3EventHandler.getBucketName(event);
 		String sourcePrefix = S3EventHandler.getObjectPrefix(event);		
-		AppLog.log.info("S3ToS3Lambda triggered by event type '" + eventType + "' from 's3://" + sourceBucket + "/" + sourcePrefix + "'");
+		AppLog.log.info("S3ToS3Lambda triggered by event type '" + eventType + "' from '" + this.getFullS3Path(sourceBucket, sourcePrefix) + "'");
 		
 		String destinationBucket = System.getenv(ENV_BUCKET);
 		String destinationPrefix = System.getenv(ENV_PREFIX);
@@ -30,12 +30,23 @@ public class S3ToS3Lambda {
 	private void transferSourceFile(String sourceBucket, String sourcePrefix, String destinationBucket, String destinationPrefix) {
 		String downloadLocation = this.getRandomFilenameLocation();
 		if(S3ClientHandler.downloadObject(sourceBucket, sourcePrefix, downloadLocation)) {
-			//upload object
+			AppLog.log.info("Successfully copied S3 object from '" + this.getFullS3Path(sourceBucket, sourcePrefix) + "' to local location '" + downloadLocation + "'");
+			this.copySourceFile(downloadLocation, destinationBucket, destinationPrefix);
 		} 
 	}
 	
 	private String getRandomFilenameLocation() {
 		String randomFilename = RandomStringUtils.randomAlphabetic(10);
 		return this.TEMP_LOCATION.concat(randomFilename);
+	}
+	
+	private void copySourceFile(String downloadLocation, String destinationBucket, String destinationPrefix) {
+		if(S3ClientHandler.uploadObject(downloadLocation, destinationBucket, destinationPrefix)) {
+			AppLog.log.info("Successfully copied S3 object from local location '" + downloadLocation + "' to '" + this.getFullS3Path(destinationBucket, destinationPrefix) + "'");
+		} 
+	}
+	
+	private String getFullS3Path(String bucketName, String objectPrefix) {
+		return "s3://" + bucketName + "/" + objectPrefix;
 	}
 }
